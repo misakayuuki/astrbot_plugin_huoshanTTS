f3e91167c69637dd8ac45bf8ffc9e4bb40a93625
苏半夏 <misa.subanxia@icloud.com>
2025-02-23 09:59:58 +0800

    chore: 更新插件说明和版本号

--- README.md
+++ README.md
@@ -4,6 +4,6 @@ AstrBot astrbot_plugin_huoshanTTS插件
 
 支持通过appid和access token调用火山声音大模型的声音复刻api
 
+在服务提供商里添加即可使用 
 # 支持
-[火山API文档]([https://astrbot.soulter.top/center/docs/%E5%BC%80%E5%8F%91/%E6%8F%92%E4%BB%B6%E5%BC%80%E5%8F%91/](https://www.volcengine.com/docs/6561/1305191#java)
-)
+[火山API文档](https://www.volcengine.com/docs/6561/1305191#java)
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
 
-@register("astrbot_plugin_huoshanTTS", "misa想变木苒苒", "添加火山TTS api支持", "1.0.0", "https://github.com/misakayuuki/astrbot_plugin_huoshanTTS")
+@register("astrbot_plugin_huoshanTTS", "misa想变木苒苒", "添加火山声音复刻 api支持。在服务提供商里添加即可使用", "1.0.1", "https://github.com/misakayuuki/astrbot_plugin_huoshanTTS")
 class MyPlugin(Star):
     def __init__(self, context: Context):
         from .huoshan import ProviderHUOSHANTTSAPI # noqa
